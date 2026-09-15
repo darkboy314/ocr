ea46b5d6bad9aa6c06b43abcb8c18edadb2ce6f0
darkboy314 <zenglin12345@gmail.com>
2026-09-15 16:44:30 +0800

    add entry for pp structure

--- src/ocr/ppocr.py
+++ src/ocr/ppocr.py
@@ -1,15 +1,18 @@
 import os
 
-from paddleocr import PaddleOCR
+from paddleocr import PaddleOCR, PPStructureV3
 
 
 class Ocr:
     def __init__(self):
-        self.pipeline = PaddleOCR(
+        self.ocr_pipeline = PaddleOCR(
             device = "gpu",
             use_doc_orientation_classify=False, # 通过 use_doc_orientation_classify 参数指定不使用文档方向分类模型
             use_doc_unwarping=False, # 通过 use_doc_unwarping 参数指定不使用文本图像矫正模型
             use_textline_orientation=False, # 通过 use_textline_orientation 参数指定不使用文本行方向分类模型   
+        )
+        self.struct_pipeline = PPStructureV3(
+            paddlex_config="PP-StructureV3.yaml" # 配置文件
         )
         return
 
@@ -17,14 +20,33 @@ class Ocr:
     def start_ocr(
         self,
         filename: str,
-        isfileoutputenable: bool,
+        isfileoutputenabled: bool,
         output_dir: str = "output",
     ):
         pipeline = self.pipeline
         result = pipeline.predict(filename)
         
         # if file output is enable, then output image and json file
-        if isfileoutputenable:
+        if isfileoutputenabled:
+            os.makedirs(output_dir, exist_ok=True)
+            for res in result:
+                res.print()
+                res.save_to_img(output_dir)
+                res.save_to_json(output_dir)
+        
+        return result
+
+    def start_struct_ocr(
+        self,
+        filename: str,
+        isfileoutputenabled: bool,
+        output_dir: str = "output",
+    ):
+        pipeline = self.struct_pipeline
+        result = pipeline.predict(filename)
+
+        # if file output is enable, then output image and json file
+        if isfileoutputenabled:
             os.makedirs(output_dir, exist_ok=True)
             for res in result:
                 res.print()
